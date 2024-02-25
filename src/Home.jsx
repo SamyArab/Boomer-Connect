1b54454b27c266a3e8c7da634a5a1457bd704649
SamyArab <samy013@outlook.com>
2024-02-25 16:49:53 -0500

    finished problem phase

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import ProductCategories from "./modules/views/ProductCategories";
 import ProductSmokingHero from "./modules/views/ProductSmokingHero";
 import AppFooter from "./modules/views/AppFooter";
 import ProductHero from "./modules/views/ProductHero";
-import ProductValues from "./modules/views/ProductValues";
+import Problem from "./modules/views/Problem";
 import ProductHowItWorks from "./modules/views/ProductHowItWorks";
 import ProductCTA from "./modules/views/ProductCTA";
 import AppAppBar from "./modules/views/AppAppBar";
@@ -14,7 +14,7 @@ function Home() {
     <>
       <AppAppBar />
       <ProductHero />
-      <ProductValues />
+      <Problem />
       <ProductCategories />
       <ProductHowItWorks />
       <ProductCTA />
