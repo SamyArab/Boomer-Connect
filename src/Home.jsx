753d922e6412cbfd09d9724e2ae85d883d8ea1da
LachezaraLaz <122994431+LachezaraLaz@users.noreply.github.com>
2024-02-25 18:40:35 -0500

    Update Home.jsx
    removed one, doesn't exist anymore

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,7 +24,6 @@ function Home() {
       <UserFlow />
       <UserJourneys />
       <StoryMap />
-      <ProductCategories />
       <ProductHowItWorks />
       <ProductCTA />
       <ProductSmokingHero />
